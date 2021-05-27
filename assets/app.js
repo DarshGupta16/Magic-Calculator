1287379741e289fb3dcc393ee26436121538d7ae
unknown <darsh.gupta.1602@gmail.com>
2021-05-27 13:56:19 +0530

    Final Project - Added Magic Calculator Functionality

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 let answer = null;
 const input = document.getElementById("calcInput");
+let pressTimer;
 
 const insert = (objToInsert) => {
   input.value += objToInsert;
@@ -7,6 +8,18 @@ const insert = (objToInsert) => {
 
 const clearInput = () => {
   input.value = null;
+  answer = null;
+};
+
+const activateMagic = () => {
+  pressTimer = window.setTimeout(() => {
+    answer = prompt("Enter the prank answer: ");
+  }, 2000);
+  return false;
+};
+
+const disableMagic = () => {
+  clearTimeout(pressTimer);
 };
 
 const findAnswer = () => {
